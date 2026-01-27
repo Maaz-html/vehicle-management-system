f9c674e780b5ab8aaa8c9901699cd0b1cbbcc8bc
Maaz-html <maazxmomin123@gmail.com>
2026-01-27 23:14:42 +0530

    final deployment

--- backend/src/utils/supabaseClient.js
+++ backend/src/utils/supabaseClient.js
@@ -1,12 +1,7 @@
-const { createClient } = require('@supabase/supabase-js');
-
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-    console.error('Missing Supabase URL or Key in environment variables.');
-}
+if (!supabaseUrl) throw new Error("supabaseUrl is required.");
+if (!supabaseKey) throw new Error("supabaseKey is required.");
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = supabase;
+export const supabase = createClient(supabaseUrl, supabaseKey);
